239bc11434c41bda1194278227dac6fc69cd43ca
Nitish kumar <109736430+NitishKumar345@users.noreply.github.com>
2025-05-07 13:48:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+ function updateTimer() {
+      const timerElement = document.getElementById('timer');
+      const now = new Date();
+      const formattedTime = now.toLocaleString();
+     timerElement.textContent = formattedTime;
+    }
+
+    updateTimer();
+    setInterval(updateTimer, 1000);
